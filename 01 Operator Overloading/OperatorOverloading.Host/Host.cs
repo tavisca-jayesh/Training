6b5813d67e39c88d9480e9e00a4f10c32139ca20
Jayesh Yeola <jyeola@tavisca.com>
2015-07-09 12:04:09 +0530

    Pushing Code for review on new branch.

---
+++ 01 Operator Overloading/OperatorOverloading.Host/Host.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OperatorOverloading.Model;
+
+namespace OperatorOverloading.Host
+{
+    class Host
+    {
+        static void Main(string[] args)
+        {
+            string inputString;
+
+            try
+            {
+                // Input
+                Console.WriteLine(Messages.InputMessage);
+                inputString = Console.ReadLine();
+
+                //calling constructor
+                Money money1 = new Money(inputString);
+
+                // Input
+                Console.WriteLine(Messages.InputMessage);
+                inputString = Console.ReadLine();
+
+                //calling constructor
+                Money money2 = new Money(inputString);
+
+                //this statement can throw exception
+                Money money3 = money1 + money2;
+                Console.WriteLine("The Amount and Currency is : {1}  {0}", money3.Currency, money3.Amount);
+            }
+            catch (Exception e)
+            {
+                //Handle the exception in this section
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 01 Operator Overloading/OperatorOverloading.Model/Money.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+
+namespace OperatorOverloading.Model
+{
+    public class Money
+    {
+        //Double Parameter constructor
+        public Money(double amount, string currency)
+        {
+            if (double.IsInfinity(amount))
+            {
+                throw new System.ArgumentException(Messages.AmountOverflow);
+            }
+            this.Amount = amount;
+            this.Currency = currency;
+        }
+
+        //Single Parameter Constructor
+        public Money(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                throw new System.ArgumentException(Messages.NullInput);
+            }
+
+            var inputarr = input.Split(' ');
+            if (inputarr.Length != 2)
+            {
+                throw new System.ArgumentException(Messages.CurrencyEmpty);
+            }
+
+            double amount;
+
+            //setting Amount
+            if (double.TryParse(inputarr[0], out amount) == false)
+            {
+                throw new System.ArgumentException(Messages.AmountInvalid);
+            }
+            this.Amount = amount;
+            //setting currency
+            if (string.IsNullOrEmpty(inputarr[1]) == true)
+            {
+                throw new System.ArgumentException(Messages.CurrencyEmpty);
+            }
+            this.Currency = inputarr[1];
+        }
+
+        //property amount
+        private double _amount;
+        public double Amount
+        {
+            get
+            {
+                return _amount;
+            }
+            set
+            {
+                if (value < 0 || value == double.MaxValue)
+                {
+                    throw new System.ArgumentException(Messages.AmountInvalid);
+                }
+                _amount = value;
+            }
+        }
+
+        //property currency
+        private string _currency;
+        public string Currency
+        {
+            get
+            {
+                return _currency;
+            }
+            private set
+            {
+                _currency = value;
+            }
+        }
+
+        //operator overloading
+        public static Money operator +(Money money1, Money money2)
+        {
+            if (money1 == null || money2 == null)
+            {
+                throw new System.ArgumentNullException(Messages.ArgumentNull);
+            }
+            if (string.Equals(money1.Currency, money2.Currency, StringComparison.OrdinalIgnoreCase))
+            {
+                return new Money(money1.Amount + money2.Amount, money1.Currency);
+            }
+            else
+            {
+                throw new System.ArgumentException(Messages.CurrencyMismatch);
+            }
+        }
+    }
+}
