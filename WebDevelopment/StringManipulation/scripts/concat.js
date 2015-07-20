97ff1a04de85c7829164a54e73da15c78fa2f5b9
Jayesh Yeola <jyeola@tavisca.com>
2015-07-20 08:39:36 +0530

    String Manipulation Added for review

---
+++ WebDevelopment/StringManipulation/scripts/concat.js
@@ -0,0 +1,9 @@
+﻿String.prototype.Concat = function (stringtoConcat) {
+    var stringMain = this,
+        ret_val = stringMain;
+    //concatinating Character by character
+    for (i = 0; i < stringtoConcat.length; i++) {
+        ret_val += stringtoConcat.charAt(i);
+    }
+    return ret_val;
+};
