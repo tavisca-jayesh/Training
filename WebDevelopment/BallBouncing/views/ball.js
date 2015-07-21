dd23f0ad1806d1c4e939a7b98f086a1066022e4f
Jayesh Yeola <jyeola@tavisca.com>
2015-07-21 10:41:14 +0530

    Ball-Bouncing Pushed for review

---
+++ WebDevelopment/BallBouncing/views/ball.js
@@ -0,0 +1,18 @@
+﻿window.ball = window.ball || {
+    xPosition: 0,
+    yPosition: 0,
+    stepX: 10,
+    stepY: 10
+};
+
+ball.bounce = function () {
+    var element = document.getElementById('circle');
+    element.style.left = ball.xPosition + "px";
+    element.style.top = ball.yPosition + "px";
+    ball.container();
+    ball.xPosition += ball.stepX;
+    ball.yPosition += ball.stepY;
+}
+window.ball.start = function () {
+    setInterval(ball.bounce, 20);
+}
---
+++ WebDevelopment/BallBouncing/views/container.js
@@ -0,0 +1,20 @@
+﻿window.ball = window.ball || {}
+
+ball.container = function () {
+
+    var height = (window.innerHeight - 50);
+    var width = (window.innerWidth - 50);
+    if (ball.yPosition >= height) {
+        ball.stepY = -10;
+    }
+    if (ball.xPosition >= width) {
+        ball.stepX = -10;
+    }
+    if (ball.yPosition <= 0) {
+        ball.stepY = 10;
+    }
+    if (ball.xPosition <= 0) {
+        ball.stepX = 10;
+    }
+}
+
